f1ab5926d77a90df630314a95df38ac8ba9a6e09
Alan Morel <alan@alanmorel.com>
2020-10-10 00:07:42 -0400

    add support for walking animations

--- client/maps/AnimatedMapObject.ts
+++ client/maps/AnimatedMapObject.ts
@@ -2,12 +2,18 @@ import * as PIXI from "pixi.js-legacy";
 import { Scene } from "pixi-scenes";
 import Map from "../maps/Map";
 import Config from "../config";
+import { AnimatedState } from "./AnimatedState";
 
 export default class AnimatedMapObject {
 
-    private activeSprite: PIXI.Sprite;
     private standingSprites: PIXI.Sprite [];
     private walkingSprites: PIXI.Sprite [];
+
+    private animationState: AnimatedState;
+    private activeSprite: PIXI.Sprite;
+    private activeSpriteIndex: number;
+    private lastSpriteChange: number;
+
     private speed: number;
     private dx: number;
     private dy: number;
@@ -29,14 +35,21 @@ export default class AnimatedMapObject {
             sprite.anchor.set(0.5, 0);
             this.walkingSprites.push(sprite);
         }
-        
-        this.activeSprite = this.standingSprites[0];
+
+        this.activeSprite = new PIXI.Sprite();
+        this.activeSprite.anchor.set(0.5, 0);
+        this.activeSprite.texture = this.standingSprites[0].texture;
         this.activeSprite.y = 50;
         this.activeSprite.texture.addListener("update", () => {
             this.activeSprite.x = this.activeSprite.texture.width / 2;
         });
 
         scene.addChild(this.activeSprite);
+
+        this.activeSpriteIndex = 0;
+        this.lastSpriteChange = Date.now();
+
+        this.animationState = AnimatedState.STANDING;
 
         this.speed = 3;
         this.dx = 0;
@@ -50,11 +63,13 @@ export default class AnimatedMapObject {
     public moveLeft() {
         this.lookLeft();
         this.dx = -this.speed;
+        this.updateAnimationState(AnimatedState.WALKING);
     }
 
     public moveRight() {
         this.lookRight();
         this.dx = this.speed;
+        this.updateAnimationState(AnimatedState.WALKING);
     }
 
     public lookLeft() {
@@ -67,13 +82,42 @@ export default class AnimatedMapObject {
 
     public stop() {
         this.dx = 0;
+        this.updateAnimationState(AnimatedState.STANDING);
     }
 
     public jump() {
         this.dy = -12;
     }
 
+    private updateAnimationState(animationState: AnimatedState) {
+        this.animationState = animationState;
+        this.activeSpriteIndex = 0;
+        this.lastSpriteChange =  Date.now() - 75;
+    }
+
+    private getSprites() {
+        if (this.animationState === AnimatedState.STANDING) {
+            return this.standingSprites;
+        } else if (this.animationState === AnimatedState.WALKING) {
+            return this.walkingSprites;
+        } else {
+            return this.standingSprites;
+        }
+    }
+
+    private updateTexture() {
+        const now = Date.now();
+        if (now - this.lastSpriteChange < 75) {
+            return;
+        }
+        const sprites = this.getSprites();
+        let nextSpriteIndex = ++this.activeSpriteIndex % sprites.length;
+        this.activeSprite.texture = sprites[nextSpriteIndex].texture;
+        this.lastSpriteChange = now;
+    }
+
     public update(map: Map) {
+        this.updateTexture();
         this.activeSprite.x += this.dx;
         this.activeSprite.y += this.dy;
         if (this.activeSprite.y < Config.height - 210) {
---
+++ client/maps/AnimatedState.ts
@@ -0,0 +1,4 @@
+export enum AnimatedState {
+    STANDING,
+    WALKING
+}
