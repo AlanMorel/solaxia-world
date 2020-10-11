b183ed02d6204bb7dd2c5a7e20e81b0f8205c248
Alan Morel <alan@alanmorel.com>
2020-10-10 22:51:46 -0400

    add optional electron desktop client

---
+++ client/Electron.ts
@@ -0,0 +1,25 @@
+import { app, BrowserWindow } from "electron";
+import Config from "./config";
+
+function createWindow() {
+    const window = new BrowserWindow({
+        title: "Solaxia World",
+        width: Config.width,
+        height: Config.height,
+        useContentSize: true,
+        backgroundColor: "#000000"
+    });
+    window.removeMenu();
+    window.on("will-resize", e => e.preventDefault());
+    window.loadURL("http://localhost:8000/");
+}
+
+app.on("ready", () => {
+    createWindow();
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
